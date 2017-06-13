fc135cc7fa8fc977753c260f3a2996b186120758
brandonboesch <Brandon.Boesch@arm.com>
2017-06-13 11:48:49 -0500

    gitignore is now applied to BUILD directories

--- slaveComms/BUILD/K64F/GCC_ARM/mbed-os/features/mbedtls/src/memory_buffer_alloc.d
+++
@@ -1,7 +0,0 @@
-BUILD/K64F/GCC_ARM/mbed-os/features/mbedtls/src/memory_buffer_alloc.o: \
- mbed-os/features/mbedtls/src/memory_buffer_alloc.c \
- BUILD/K64F/GCC_ARM/mbed_config.h \
- mbed-os/features/mbedtls/inc/mbedtls/config.h \
- mbed-os/features/mbedtls/platform/inc/platform_mbed.h mbedtls_config.h \
- mbed-os/features/mbedtls/inc/mbedtls/check_config.h \
- mbed-os/features/mbedtls/inc/mbedtls/check_config.h
--- slaveComms/BUILD/K64F/GCC_ARM/mbed-os/features/mbedtls/src/ssl_cache.d
+++
@@ -1,7 +0,0 @@
-BUILD/K64F/GCC_ARM/mbed-os/features/mbedtls/src/ssl_cache.o: \
- mbed-os/features/mbedtls/src/ssl_cache.c \
- BUILD/K64F/GCC_ARM/mbed_config.h \
- mbed-os/features/mbedtls/inc/mbedtls/config.h \
- mbed-os/features/mbedtls/platform/inc/platform_mbed.h mbedtls_config.h \
- mbed-os/features/mbedtls/inc/mbedtls/check_config.h \
- mbed-os/features/mbedtls/inc/mbedtls/check_config.h
