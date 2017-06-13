046750440d6c25d0cad5b14da69c2d27ba02fbac
brandonboesch <Brandon.Boesch@arm.com>
2017-06-13 11:38:16 -0500

    added sample motion project

---
+++ motion/main.cpp
@@ -0,0 +1,23 @@
+// Filename: main.cpp
+// Built by: Brandon Boesch
+// Date    : June 12th, 2017
+
+#include "mbed.h"
+#include "FXOS8700CQ.h"
+
+FXOS8700CQ device(I2C_SDA,I2C_SCL);      // create object and specifiy pins
+Serial pc(USBTX, USBRX);                 //use these pins for serial coms.
+int main(){
+  pc.baud(115200);                       //Set baudrate here.
+  device.init();                         // call initialization method
+  while (1) {
+    // poll the sensor and get the values, storing in a struct
+    Data values = device.get_values();
+        
+    // print each struct member over serial
+    printf("ax = %f ay = %f az = %f | mx = %f my = %f mz = %f\n\r"
+           ,values.ax, values.ay, values.az
+           ,values.mx, values.my, values.mz);
+    wait(1);
+  }
+}
