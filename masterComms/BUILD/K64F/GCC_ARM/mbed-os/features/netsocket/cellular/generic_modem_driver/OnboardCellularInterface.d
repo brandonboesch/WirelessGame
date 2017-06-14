183c3393dba2630e889edc9c79f00dd31762d231
brandonboesch <Brandon.Boesch@arm.com>
2017-06-14 16:16:59 -0500

    removed BUILD from masterComms

--- masterComms/BUILD/K64F/GCC_ARM/mbed-os/features/netsocket/cellular/generic_modem_driver/OnboardCellularInterface.d
+++
@@ -1,4 +0,0 @@
-BUILD/K64F/GCC_ARM/mbed-os/features/netsocket/cellular/generic_modem_driver/OnboardCellularInterface.o: \
- mbed-os/features/netsocket/cellular/generic_modem_driver/OnboardCellularInterface.cpp \
- BUILD/K64F/GCC_ARM/mbed_config.h \
- mbed-os/features/netsocket/cellular/generic_modem_driver/OnboardCellularInterface.h
--- masterComms/BUILD/K64F/GCC_ARM/mbed-os/rtos/rtx5/TARGET_CORTEX_M/rt_OsEventObserver.d
+++
@@ -1,4 +0,0 @@
-BUILD/K64F/GCC_ARM/mbed-os/rtos/rtx5/TARGET_CORTEX_M/rt_OsEventObserver.o: \
- mbed-os/rtos/rtx5/TARGET_CORTEX_M/rt_OsEventObserver.c \
- BUILD/K64F/GCC_ARM/mbed_config.h \
- mbed-os/rtos/rtx5/TARGET_CORTEX_M/rt_OsEventObserver.h
