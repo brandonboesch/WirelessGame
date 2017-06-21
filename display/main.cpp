ec2d4ac5e94c3f9db5925ca25593480e9e9c981c
brandonboesch <Brandon.Boesch@arm.com>
2017-06-21 14:48:55 -0500

    cleaned up started code for ST7735

--- display/main.cpp
+++ display/main.cpp
@@ -1,28 +1,39 @@
 #include "mbed.h"
 #include "Adafruit_ST7735.h"
 
+// There are two different SPI ports that can be utilized on the K64F. Update the tft global below to match whichever pins your screen is connected to.
+
+// ST7735 Interface (Option 1)
+//   LITE connected to +3.3 V
+//   MISO connected to PTD7
+//   SCK connected to PTD5
+//   MOSI connected to PTD6
+//   TFT_CS connected to PTD4
+//   CARD_CS unconnected 
+//   D/C connected to PTC18
+//   RESET connected to +3.3 V
+//   VCC connected to +3.3 V
+//   Gnd connected to ground
+
+// ST7735 Interface (Option 2)
+//   LITE connected to +3.3 V
+//   MISO connected to D12
+//   SCK connected to D13
+//   MOSI connected to D11
+//   TFT_CS connected to D10
+//   CARD_CS unconnected 
+//   D/C connected to D8
+//   RESET connected to D9
+//   VCC connected to +3.3 V
+//   Gnd connected to ground
 
 
-////////////////////////////////////////////////////////////////////////////////
-// ST7735 Interface
-
-// LITE connected to +3.3 V
-// MISO connected to D12
-// SCK connected to D13
-// MOSI connected to D11
-// TFT_CS connected to D10
-// CARD_CS unconnected 
-// Data/Command connected to D8
-// RESET connected to D9
-// VCC connected to +3.3 V
-// Gnd connected to ground
-
-Adafruit_ST7735 tft(D11, D12, D13, D10, D8, D9); // MOSI, MISO, SCLK, TFT_CS, D/C, RESET
+Adafruit_ST7735 tft(PTD6, PTD7, PTD5, PTD4, PTC18, PTC4); // MOSI, MISO, SCK, TFT_CS, D/C, RESET
 
 
 int main(void){
   // Use this initializer if you're using a 1.8" TFT
   tft.initR(INITR_BLACKTAB);   // initialize a ST7735S chip, black tab
 
-  tft.fillScreen(ST7735_BLUE);
+  tft.fillScreen(ST7735_GREEN);
 }
